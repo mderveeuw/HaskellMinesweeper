67f497405162313b5033c04946e6d9085bde7dea
Michiel Derveeuw <mderveeuw@gmail.com>
2018-08-17 00:37:48 +0200

    Implement mark

--- Minesweeper.hs
+++ Minesweeper.hs
@@ -2,13 +2,14 @@
 -- Has an Int having values 1 - 8 indicating the count 
 -- of surrounding mines or 9 if cell contains mine
 -- Has a Bool which is True if cell is shown
--- Has another Bool which is True if cell is marked as mine 
+-- Has a Bool which is True if cell is marked as mine 
 data Cell = Cell Int Bool Bool deriving (Show)
 
 -- Data type for a field
 -- Has an Int equal to field width
 -- Has an Int equal to field height
 -- Has an Int equal to amount of mines in field
+-- Has a 2-dimensional list of cells representing field
 -- TODO: safe constructor?
 data Field = Field Int Int Int [[Cell]] deriving (Show)
 
@@ -27,7 +28,10 @@ update x y cell (Field w h m f) =
     Field w h m (replace y (replace x cell (f !! y)) f)
 
 show :: Int -> Int -> Field -> Field
-show x y field = undefined 
+show x y field = undefined
 
 mark :: Int -> Int -> Field -> Field
-mark x y field = undefined
+mark x y field@(Field w h m f) = update x y markedCell field
+                    where Cell mineCount shown _ = f !! y !! x
+                          markedCell = Cell mineCount shown True
+                          
