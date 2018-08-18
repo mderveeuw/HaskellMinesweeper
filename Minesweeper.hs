7437ba458d78298dc5240f48ce98c46f1f7a367f
Michiel Derveeuw <mderveeuw@gmail.com>
2018-08-18 13:24:21 +0200

    Implement show

--- Minesweeper.hs
+++ Minesweeper.hs
@@ -27,11 +27,35 @@ update :: Int -> Int -> Cell -> Field -> Field
 update x y cell (Field w h m f) = 
     Field w h m (replace y (replace x cell (f !! y)) f)
 
+getCell :: Int -> Int -> Field -> Cell
+getCell x y (Field _ _ _ f) = f !! y !! x
+
 show :: Int -> Int -> Field -> Field
-show x y field = undefined
+show x y field@(Field w h m f) = if inBounds x y field && not shown then 
+                                     if mineCount == 0 then
+                                         fieldWest
+                                     else
+                                         field'
+                                 else
+                                     field
+                                 where inBounds x y (Field w h _ _) 
+                                            = 0 <= x && x < w && 0 <= y && y < h
+                                       Cell mineCount shown marked 
+                                            = getCell x y field
+                                       field' 
+                                            = update x y 
+                                              (Cell mineCount True marked) field
+                                       fieldNorth 
+                                            = Main.show x (y - 1) field'
+                                       fieldEast 
+                                            = Main.show (x + 1) y fieldNorth
+                                       fieldSouth 
+                                            = Main.show x (y + 1) fieldEast
+                                       fieldWest 
+                                            = Main.show (x - 1) y fieldSouth
 
 mark :: Int -> Int -> Field -> Field
 mark x y field@(Field w h m f) = update x y markedCell field
-                    where Cell mineCount shown _ = f !! y !! x
+                    where Cell mineCount shown _ = getCell x y field
                           markedCell = Cell mineCount shown True
                           
